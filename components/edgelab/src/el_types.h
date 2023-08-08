c6e31d5e06070fe6d67e582f6e182cbc1f8946b2
Unbinilium <15633984+Unbinilium@users.noreply.github.com>
2023-08-09 00:35:56 +0800

    feat: model binary format specification

--- components/edgelab/src/el_types.h
+++ components/edgelab/src/el_types.h
@@ -35,26 +35,25 @@
 
 #ifdef __cplusplus
 extern "C" {
-namespace edgelab {
 #endif
 
 /*------------------------------------------------------------------*/
 /* CONSTANTS
  *------------------------------------------------------------------*/
 enum {
-    EL_OK = 0,       // success
-    EL_LOGE = -1,   // general error
-    EL_ETIMOUT = -2, // timeout
-    EL_EIO = -3,     // IO error
-    EL_EINVAL = -4,  // invalid argument
-    EL_ENOMEM = -5,  // out of memory
-    EL_EBUSY = -6,   // busy
-    EL_ENOTSUP = -7, // not supported
-    EL_EPERM = -8,   // operation not permitted
-    EL_EAGAIN = -9,  // try again
+    EL_OK      = 0,  // success
+    EL_AGAIN   = 1,  // try again
+    EL_ELOG    = 2,  // general error
+    EL_ETIMOUT = 3,  // timeout
+    EL_EIO     = 4,  // IO error
+    EL_EINVAL  = 5,  // invalid argument
+    EL_ENOMEM  = 6,  // out of memory
+    EL_EBUSY   = 7,  // busy
+    EL_ENOTSUP = 8,  // not supported
+    EL_EPERM   = 9,  // operation not permitted
 };
 
-typedef int EL_ERR;
+typedef uint8_t el_err_code_t;
 
 /*------------------------------------------------------------------*/
 /* STRUCTURES
@@ -62,16 +61,16 @@ typedef int EL_ERR;
 
 typedef struct EL_ATTR_PACKED {
     size_t size;
-    int *dims;
+    int*   dims;
 } el_shape_t;
 
 typedef struct EL_ATTR_PACKED {
-    float scale;
+    float   scale;
     int32_t zero_point;
 } el_quant_param_t;
 
 typedef struct EL_ATTR_PACKED {
-    void *pool;
+    void*  pool;
     size_t size;
 } el_memory_pool_t;
 
@@ -80,9 +79,15 @@ typedef struct EL_ATTR_PACKED {
     uint16_t y;
     uint16_t w;
     uint16_t h;
-    uint8_t score;
-    uint8_t target;
+    uint8_t  score;
+    uint8_t  target;
 } el_box_t;
+
+typedef struct EL_ATTR_PACKED {
+    uint16_t x;
+    uint16_t y;
+    uint8_t  target;
+} el_point_t;
 
 typedef struct EL_ATTR_PACKED {
     uint16_t score;
@@ -91,13 +96,13 @@ typedef struct EL_ATTR_PACKED {
 
 typedef struct EL_ATTR_PACKED {
     size_t index;
-    void *data;
+    void*  data;
     size_t size;
 } el_tensor_t;
 
 typedef struct EL_ATTR_PACKED {
-    el_tensor_t *tensor;
-    size_t length;
+    el_tensor_t* tensor;
+    size_t       length;
 } el_input_t;
 
 typedef enum {
@@ -118,10 +123,10 @@ typedef enum {
 } el_pixel_rotate_t;
 
 typedef struct EL_ATTR_PACKED {
-    uint8_t *data;
-    size_t size;
-    uint32_t width;
-    uint32_t height;
+    uint8_t*          data;
+    size_t            size;
+    uint32_t          width;
+    uint32_t          height;
     el_pixel_format_t format;
     el_pixel_rotate_t rotate;
 } el_img_t;
@@ -131,9 +136,52 @@ typedef struct EL_ATTR_PACKED {
     uint32_t height;
 } el_res_t;
 
+typedef struct EL_ATTR_PACKED {
+    uint8_t id;
+    uint8_t type;
+    uint8_t parameters[6];
+} el_sensor_t;
+
+/**
+ * @brief Algorithm Types
+ */
+typedef enum { UNDEFINED = 0u, FOMO = 1u, PFLD = 2u, YOLO = 3u } el_algorithm_type_t;
+
+/**
+ * @brief Model Header Specification
+ * @details
+ *      [ 24 bits magic code | 4 bits id | 4 bits type | 24 bits size (unsigned) | 8 bits unused padding ]
+ *      big-endian in file
+ */
+typedef union EL_ATTR_PACKED {
+    unsigned char b1[8];
+    uint32_t      b4[2];
+} el_model_header_t;
+
+/**
+ * @brief Mdoel Info Specification
+ * @details 
+ *      valid id range [1, 15]:
+ *          0 -> undefined
+ *          each model should have a unique id
+ *      valid type range [1, 15]:
+ *          0 -> undefined
+ *          1 -> FOMO
+ *          2 -> PFLD
+ *          3 -> YOLO
+ */
+typedef struct EL_ATTR_PACKED {
+    uint8_t             id;
+    el_algorithm_type_t type;
+    uint32_t            addr_flash;
+    uint32_t            size;
+    const uint8_t*      addr_memory;
+} el_model_info_t;
+
+typedef uint8_t el_model_id_t;
+
 #ifdef __cplusplus
-}
 }
 #endif
 
-#endif /* _EL_TYPES_H_ */
+#endif

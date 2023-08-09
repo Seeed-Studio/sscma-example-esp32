04acb16b9fb80e60e405935ca28b6424ab1e252f
nullptr <nullptr@localhost>
2023-08-09 10:17:13 +0800

    chore: cleanup cv utils

--- components/edgelab/src/utils/el_cv.h
+++ components/edgelab/src/utils/el_cv.h
@@ -35,65 +35,34 @@ extern "C" {
 namespace edgelab {
 #endif
 
-/**
- * @brief Convert image format
- *
- * @param src
- * @param dst
- * @return el_err_code_t
- */
+el_err_code_t yuv422p_to_rgb(const el_img_t* src, el_img_t* dst);
+
 el_err_code_t el_img_convert(const el_img_t* src, el_img_t* dst);
 
-/**
- * @brief Crop image
- *
- * @param src
- * @param dst
- * @param box
- * @return void
- */
 void el_crop_image(const el_img_t* src, el_img_t* dst, const el_box_t* box);
 
-/**
- * @brief Resize image
- *
- * @param src
- * @param dst
- * @param dst_width
- * @param dst_height
- * @param keep_ratio
- * @return void
- */
 void el_resize_image(
   const el_img_t* src, el_img_t* dst, uint32_t dst_width, uint32_t dst_height, bool keep_ratio = false);
 
-/**
- * @brief draw box
- *
- * @param img
- * @param box
- * @param show_text
- * @return void
- */
 void el_draw_box(el_img_t* img, const el_box_t* box, bool show_text = false);
 
-/**
- * @brief draw box
- *
- * @param img
- * @param x
- * @param y
- * @param w
- * @param h
- * @param color
- * @return void
- */
 void el_draw_rect(
   el_img_t* img, uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint32_t color, uint16_t thickness = 1);
 
+void el_fill_rect(el_img_t* img, uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint32_t color);
+
+void          el_draw_point(el_img_t* img, uint16_t x, uint16_t y, uint32_t color);
 el_err_code_t rgb_to_rgb(const el_img_t* src, el_img_t* dst);
 
+void el_draw_h_line(el_img_t* img, uint16_t x0, uint16_t x1, uint16_t y, uint32_t color);
+
+void el_draw_v_line(el_img_t* img, uint16_t x, uint16_t y0, uint16_t y1, uint32_t color);
+
+#ifdef CONFIG_EL_LIB_JPEGENC
+
 el_err_code_t rgb_to_jpeg(const el_img_t* src, el_img_t* dst);
+
+#endif
 
 #ifdef __cplusplus
 }  // namespace edgelab

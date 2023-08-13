74cbe023606a092206cb890c2e35ad65f8889e73
nullptr <nullptr@localhost>
2023-08-13 16:04:45 +0800

    feat: add task executor and its demo

---
+++ examples/at_server_demo/main/task_executor.hpp
@@ -0,0 +1,70 @@
+
+#pragma once
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/semphr.h>
+
+#include <atomic>
+#include <functional>
+#include <memory>
+#include <queue>
+#include <thread>
+#include <utility>
+
+class TaskExecutor {
+   public:
+    TaskExecutor()
+        : __task_queue_lock(xSemaphoreCreateCounting(1, 1)),
+          __task_stop_requested(false),
+          __worker_thread_stop_requested(false) {}
+
+    ~TaskExecutor() {
+        stop();
+        vSemaphoreDelete(__task_queue_lock);
+    }
+
+    void start() { __worker_thread = std::thread(&TaskExecutor::run, this); }
+
+    void stop() {
+        __worker_thread_stop_requested.store(true, std::memory_order_relaxed);
+        __worker_thread.join();
+    }
+
+    void add_task(std::function<void(std::atomic<bool>&)> task) {
+        m_lock();
+        __task_queue.push(task);
+        __task_stop_requested.store(true, std::memory_order_relaxed);
+        m_unlock();
+    }
+
+   protected:
+    inline void m_lock() { xSemaphoreTake(__task_queue_lock, portMAX_DELAY); }
+    inline void m_unlock() { xSemaphoreGive(__task_queue_lock); }
+
+   private:
+    std::queue<std::function<void(std::atomic<bool>&)>> __task_queue;
+    std::thread                                         __worker_thread;
+
+    SemaphoreHandle_t __task_queue_lock;
+    std::atomic<bool> __task_stop_requested;
+    std::atomic<bool> __worker_thread_stop_requested;
+
+    void run() {
+        while (!__worker_thread_stop_requested.load(std::memory_order_relaxed)) {
+            std::function<void(std::atomic<bool>&)> task;
+            {
+                m_lock();
+                if (!__task_queue.empty()) {
+                    task = std::move(__task_queue.front());
+                    __task_queue.pop();
+                }
+                m_unlock();
+            }
+            if (task) {
+                __task_stop_requested.store(false, std::memory_order_release);
+                task(__task_stop_requested);
+            }
+            vTaskDelay(20 / portTICK_PERIOD_MS);
+        }
+    }
+};
---
+++ examples/task_executor_demo/main/app_main.cpp
@@ -0,0 +1,31 @@
+
+
+#include <stdio.h>
+
+#include "task_executor.hpp"
+
+extern "C" void app_main() {
+    TaskExecutor* executor = new TaskExecutor();
+
+    printf("Starting task thread...\n");
+    executor->start();
+    printf("Task thread started...\n");
+    
+    printf("Waiting for tasks...\n");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    for (int i = 0; i < 5; ++i) {
+        printf("Adding task %d...\n", i);
+        executor->add_task([i](std::atomic<bool>& stop_token) {
+            printf("Task %d executing ->\n\texiting after %d seconds\n", i, 5 - i);
+            vTaskDelay((5 - i) * 1000 / portTICK_PERIOD_MS);
+            printf("\ttask %d done ->\n", i);
+        });
+    }
+    printf("Waiting all tasks finished...\n");
+
+    // TODO: move freeRTOS, ESP related function call to EdgeLab
+    for (;;) vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    delete executor;
+}

03bd41df676710414fa921c7d8efd2d142c7d054
nullptr <nullptr@localhost>
2023-09-06 20:59:20 +0800

    refactor: increase cmd buffer size from 1024 bytes to 4096 bytes

--- examples/at_server_demo/main/at_definations.hpp
+++ examples/at_server_demo/main/at_definations.hpp
@@ -5,7 +5,7 @@
 
 #define kTensorArenaSize (1024 * 1024)
 
-#define CMD_MAX_LENGTH   (1024)
+#define CMD_MAX_LENGTH   (4096)
 
 #define REPLY_CMD_HEADER "\r{\"type\": 0, "
 #define REPLY_EVT_HEADER "\r{\"type\": 1, "

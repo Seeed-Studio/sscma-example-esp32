b89340f8dbe7bc841dd6ed20c347419aad776f53
nullptr <nullptr@localhost>
2024-11-05 19:08:19 +0800

    fix: uart gpio pin num

--- components/sscma-micro-porting/porting/espressif/esp32s3/ma_transport_serial.cpp
+++ components/sscma-micro-porting/porting/espressif/esp32s3/ma_transport_serial.cpp
@@ -11,22 +11,22 @@
 #include "driver/uart.h"
 #include <driver/usb_serial_jtag.h>
 
-#define ECHO_TEST_TXD (6)
-#define ECHO_TEST_RXD (7)
-#define ECHO_TEST_RTS (UART_PIN_NO_CHANGE)
-#define ECHO_TEST_CTS (UART_PIN_NO_CHANGE)
+#define _TEST_TXD (GPIO_NUM_43)
+#define _TEST_RXD (GPIO_NUM_44)
+#define _TEST_RTS (UART_PIN_NO_CHANGE)
+#define _TEST_CTS (UART_PIN_NO_CHANGE)
 
-#define ECHO_UART_PORT_NUM      (UART_NUM_1)
-#define ECHO_UART_BAUD_RATE     (115200)
-#define ECHO_TASK_STACK_SIZE    (4096)
+#define _UART_PORT_NUM      (UART_NUM_1)
+#define _UART_BAUD_RATE     (921600)
+#define _TASK_STACK_SIZE    (4096)
 
 namespace ma {
 
-static auto _driver_config          = usb_serial_jtag_driver_config_t{.tx_buffer_size = 8192, .rx_buffer_size = 8192};
+static auto _driver_config          = usb_serial_jtag_driver_config_t{.tx_buffer_size = 4096 * 4, .rx_buffer_size = 2048};
 static SPSCRingBuffer<char>* _rb_rx = nullptr;
 
 static uart_config_t uart_config = {
-    .baud_rate  = ECHO_UART_BAUD_RATE,
+    .baud_rate  = _UART_BAUD_RATE,
     .data_bits  = UART_DATA_8_BITS,
     .parity     = UART_PARITY_DISABLE,
     .stop_bits  = UART_STOP_BITS_1,
@@ -49,15 +49,15 @@ ma_err_t Serial::init(const void* config) {
     (void)config;
 
 
-    if (uart_driver_install(ECHO_UART_PORT_NUM, _driver_config.rx_buffer_size , 0, 0, NULL, intr_alloc_flags) != ESP_OK) {
+    if (uart_driver_install(_UART_PORT_NUM, _driver_config.rx_buffer_size , 0, 0, NULL, intr_alloc_flags) != ESP_OK) {
         return MA_EIO;
     }
 
-    if (uart_param_config(ECHO_UART_PORT_NUM, &uart_config) != ESP_OK) {
+    if (uart_param_config(_UART_PORT_NUM, &uart_config) != ESP_OK) {
         return MA_EIO;
     }
 
-    if (uart_set_pin(ECHO_UART_PORT_NUM, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS) != ESP_OK) {
+    if (uart_set_pin(_UART_PORT_NUM, _TEST_TXD, _TEST_RXD, _TEST_RTS, _TEST_CTS) != ESP_OK) {
         return MA_EIO;
     }
 
@@ -75,12 +75,12 @@ void Serial::deInit() {
         return;
     }
 
+    uart_driver_delete(_UART_PORT_NUM);
+
     if (_rb_rx) {
         delete _rb_rx;
         _rb_rx = nullptr;
     }
-
-    uart_driver_delete(ECHO_UART_PORT_NUM);
 
     m_initialized = false;
 }
@@ -99,13 +99,10 @@ size_t Serial::send(const char* data, size_t length) {
     while (length) {
         size_t bytes_to_send{length < _driver_config.tx_buffer_size ? length : _driver_config.tx_buffer_size};
 
-        sent += uart_write_bytes(ECHO_UART_PORT_NUM, data + pos_of_bytes, bytes_to_send);
+        sent += uart_write_bytes(_UART_PORT_NUM, data + pos_of_bytes, bytes_to_send);
         pos_of_bytes += bytes_to_send;
         length -= bytes_to_send;
     }
-
-    // ! https://github.com/espressif/esp-idf/issues/13162
-    fsync(fileno(stdout));
 
     return sent;
 }
@@ -115,7 +112,7 @@ size_t Serial::flush() {
         return 0;
     }
 
-    return fsync(fileno(stdout));
+    return 0;
 }
 
 size_t Serial::receive(char* data, size_t length) {
@@ -126,7 +123,7 @@ size_t Serial::receive(char* data, size_t length) {
     size_t rlen   = 0;
     char rbuf[32] = {0};  // Most commands are less than 32 bytes long
     do {
-        rlen = uart_read_bytes(ECHO_UART_PORT_NUM, rbuf, sizeof(rbuf), 10 / portTICK_PERIOD_MS);
+        rlen = uart_read_bytes(_UART_PORT_NUM, rbuf, sizeof(rbuf), 10 / portTICK_PERIOD_MS);
         _rb_rx->push(rbuf, rlen);
     } while (rlen > 0);
 
@@ -142,7 +139,7 @@ size_t Serial::receiveIf(char* data, size_t length, char delimiter) {
     size_t rlen   = 0;
     char rbuf[32] = {0};  // Most commands are less than 32 bytes long
     do {
-        rlen = uart_read_bytes(ECHO_UART_PORT_NUM, rbuf, sizeof(rbuf), 10 / portTICK_PERIOD_MS);
+        rlen = uart_read_bytes(_UART_PORT_NUM, rbuf, sizeof(rbuf), 10 / portTICK_PERIOD_MS);
         for (size_t i = 0; i < rlen; i++) {
            printf("%c", rbuf[i]);
         }
